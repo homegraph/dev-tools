a4f8ebfe85917ca8e912012a24995f09591210cd
Paolo Tremadio <hello@paolotremadio.com>
2023-08-26 16:28:01 +0100

    Lint rules for typescript

--- packages/eslint-config-hg-base/index.js
+++ packages/eslint-config-hg-base/index.js
@@ -1,8 +1,17 @@
-const overrides = require('./overrides');
-
 module.exports = {
   extends: [
     'eslint-config-airbnb-base',
+    'eslint-config-airbnb-typescript/base',
   ].map(require.resolve),
-  ...overrides,
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'max-len': 0,
+    'no-multiple-empty-lines': 0,
+    'import/no-import-module-exports': 0,
+    'import/no-relative-packages': 0,
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 2021,
+  },
 };
