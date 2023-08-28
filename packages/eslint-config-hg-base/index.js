a867e6d1a6d3c7f7e50eb79d41c109bf5f3631a6
Paolo Tremadio <hello@paolotremadio.com>
2023-08-28 23:54:03 +0100

    Test stricter linting

--- packages/eslint-config-hg-base/index.js
+++ packages/eslint-config-hg-base/index.js
@@ -1,7 +1,12 @@
 module.exports = {
+  plugins: ["@typescript-eslint"],
   extends: [
     'eslint-config-airbnb-base',
     'eslint-config-airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
   ].map(require.resolve),
   parser: '@typescript-eslint/parser',
   rules: {
@@ -10,9 +15,17 @@ module.exports = {
     'import/no-import-module-exports': 0,
     'import/no-relative-packages': 0,
     'import/prefer-default-export': 1,
+    '@typescript-eslint/ban-ts-comment': 1,
+    '@typescript-eslint/no-explicit-any': 0,
   },
   parserOptions: {
     project: './tsconfig.json',
     ecmaVersion: 2021,
   },
+  overrides: [
+    {
+      files: ['*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked'],
+    },
+  ],
 };
